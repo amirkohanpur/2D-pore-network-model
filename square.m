619cf399df7614964e776758056cf40b37057e9a
Amir H. Kohanpur <60991329+amirkohanpur@users.noreply.github.com>
2020-02-19 13:41:29 -0600

    Delete square.m

--- square.m
+++
@@ -1,7 +0,0 @@
-% Plot a square with (x,y) center and side length l
-function square(x,y,l)
-line([x-(l/2),x+(l/2)],[y+(l/2),y+(l/2)]);
-line([x-(l/2),x-(l/2)],[y+(l/2),y-(l/2)]);
-line([x+(l/2),x+(l/2)],[y-(l/2),y+(l/2)]);
-line([x+(l/2),x-(l/2)],[y-(l/2),y-(l/2)]);
-end
