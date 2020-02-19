02ab0d681a18b66eec97d215252444a30367ab08
Amir H. Kohanpur <60991329+amirkohanpur@users.noreply.github.com>
2020-02-19 13:41:58 -0600

    Delete rect.m

--- rect.m
+++
@@ -1,9 +0,0 @@
-% Plot a rectangle
-% (x,y) is center
-% lx is horizontal side and ly is vertical side
-function rect(x,y,lx,ly)
-line([x-(lx/2),x+(lx/2)],[y+(ly/2),y+(ly/2)]);
-line([x-(lx/2),x-(lx/2)],[y+(ly/2),y-(ly/2)]);
-line([x+(lx/2),x+(lx/2)],[y-(ly/2),y+(ly/2)]);
-line([x+(lx/2),x-(lx/2)],[y-(ly/2),y-(ly/2)]);
-end
