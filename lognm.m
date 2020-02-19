1db309001442cf4092ecbc3ae4d8547f1aeb91a2
Amir H. Kohanpur <60991329+amirkohanpur@users.noreply.github.com>
2020-02-19 13:42:30 -0600

    Delete lognm.m

--- lognm.m
+++
@@ -1,7 +0,0 @@
-function matrix=lognm(nx,ny,min,max,mean,sd)
-mu = log((mean^2)/sqrt(sd+mean^2));
-sigma = sqrt(log(sd/(mean^2)+1));
-dist = makedist('LogNormal','mu',mu,'sigma',sigma);
-trdist=truncate(dist,min,max);
-matrix=random(trdist,[nx,ny]);
-end
